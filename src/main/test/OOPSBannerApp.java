bd274c3fb0c47ec4da025032468d07503bcf0961
Yuvaraj-MC <yuvaraj.mc@bridgelabz.com>
2026-01-29 16:17:43 +0530

    [yuvaraj] Initial commit: Setup OOPSBannerApp project

---
+++ src/main/test/OOPSBannerApp.java
@@ -0,0 +1,17 @@
+
+/**
+ * OOPSBannerApp
+ * <p>
+ * OOPS Banner Display Application
+ * <p>
+ * This is the initial setup for the OOPS Banner App project.
+ *
+ * @author Developer
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+        // Initial project setup
+    }
+}
